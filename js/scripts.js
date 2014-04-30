5c48d94c34442706fc61b8877001eed1b015d55d
Jordan Dalgarn <adidasfox@hotmail.com>
2014-04-30 14:37:12 -0700

    cool new js stuff

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,26 @@
 // My Scripts
 $(document).foundation();
+
+//Animate.css Stuff
+
+$('h1').click(function(){
+
+	$('h1').addClass('animated rubberBand');
+	//console.log('Click Me');
+
+});
+
+// Transit.js Stuff
+
+$('img').click(function(){
+
+	$('p').transition({
+
+		x: 200,
+		y: 200
+
+	});
+
+});
+
+//unheap.com
